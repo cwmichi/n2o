4fe2265b4681c9797ef3795fd7a856111a3d4947
Michael Scharrer <michael.scharrer@cordaware.com>
2026-01-13 15:28:34 +0100

    n2o_secret

--- src/handlers/n2o_error.erl
+++ src/handlers/n2o_error.erl
@@ -13,17 +13,9 @@
 %         n2o_document:run/1:15
 
 stack(Error, Reason) ->
-    Stacktrace = [case A of
-         { Module,Function,Arity,Location} ->
-             { Module,Function,Arity,proplists:get_value(line, Location) };
-         Else -> Else end
-    || A <- erlang:get_stacktrace()],
-    [Error, Reason, Stacktrace].
+    [Error, Reason, []].
 
 
 error_page(Class,Error) ->
     io_lib:format("ERROR:  ~w:~w~n~n",[Class,Error]) ++
-    "STACK: " ++
-    [ wf:render([io_lib:format("\t~w:~w/~w:~w",
-        [ Module,Function,Arity,proplists:get_value(line, Location) ]),"\n"])
-    ||  { Module,Function,Arity,Location} <- erlang:get_stacktrace() ].
+    "STACK: ".
