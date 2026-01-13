3aca98ff3fbbf40d0e1758535fd406b75098c430
Michael Scharrer <michael.scharrer@cordaware.com>
2026-01-13 14:02:58 +0100

    n2o_secret

--- src/handlers/n2o_secret.erl
+++ src/handlers/n2o_secret.erl
@@ -8,8 +8,8 @@ pickle(Data) ->
     Message = term_to_binary({Data,os:timestamp()}),
     Padding = size(Message) rem 16,
     Bits = (16-Padding)*8, Key = secret(), IV = crypto:rand_bytes(16),
-    Cipher = crypto:block_encrypt(aes_cbc128,Key,IV,<<Message/binary,0:Bits>>),
-    Signature = crypto:hmac(sha256,Key,<<Cipher/binary,IV/binary>>),
+    Cipher = crypto:crypto_one_time(aes_128_cbc,Key,IV,<<Message/binary,0:Bits>>,true),
+    Signature = crypto:mac(hmac,sha256,Key,<<Cipher/binary,IV/binary>>),
     base64:encode(<<IV/binary,Signature/binary,Cipher/binary>>).
 
 secret() -> wf:config(n2o,secret,<<"ThisIsClassified">>).
@@ -18,7 +18,7 @@ depickle(PickledData) ->
     try Key = secret(),
         Decoded = base64:decode(wf:to_binary(PickledData)),
         <<IV:16/binary,Signature:32/binary,Cipher/binary>> = Decoded,
-        Signature = crypto:hmac(sha256,Key,<<Cipher/binary,IV/binary>>),
-        {Data,_Time} = binary_to_term(crypto:block_decrypt(aes_cbc128,Key,IV,Cipher),[safe]),
+        Signature = crypto:mac(hmac,sha256,Key,<<Cipher/binary,IV/binary>>),
+        {Data,_Time} = binary_to_term(crypto:crypto_one_time(aes_128_cbc,Key,IV,Cipher,false),[safe]),
         Data
     catch E:R -> wf:info(?MODULE,"Depicke Error: ~p",[{E,R}]), undefined end.
